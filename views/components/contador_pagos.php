d04ddef4465082e6bdc9ae04cf37d5058e50339e
ivnaquino <ivn.univ@gmail.com>
2013-06-19 10:42:33 -0500

    Denegacion de archivos

--- views/components/contador_pagos.php
+++ views/components/contador_pagos.php
@@ -9,8 +9,24 @@ if (isset($_POST['aceptar'])) {
 		'monto' => $_POST['monto'],
 		'estado' => 'pagado'
 		);
+	//$pago_error = Pago::pagar($pago_actual);
+	//echo $pago_error;
+	echo "Aceptar";
+}
+if (isset($_POST['setAlumno'])) {
+	$alumno_pago = Alumno::buscar($_POST['matricula']);
+}
+if (isset($_POST['aplazar'])) {
+	$pago_actual = array(
+		'matricula' => $_POST['matricula'],
+		'fecha' => date('Y-m-d'),
+		'concepto' => $_POST['concepto'],
+		'recargo' => 0,
+		'monto' => $_POST['monto'],
+		'estado' => 'aplazado'
+		);
 	$pago_error = Pago::pagar($pago_actual);
-	echo $pago_error;
+	//echo $pago_error;
 }
 
  ?>
@@ -23,20 +39,22 @@ if (isset($_POST['aceptar'])) {
 					<div class="control-group">
 						<label for="matricula" class="control-label">Matricula: </label>
 						<div class="controls">
-							<input type="text" id='matricula' class='span4' name='matricula' placeholder='Matricula'>
-							<button type='submit' name='setear' class='btn btn-link'>Establecer</button>
+							<input type="text" id='matricula' class='span4' name='matricula' placeholder='Matricula' 
+							value="<?php if(isset($alumno_pago)){ echo $alumno_pago['matricula']; }else{echo "";} ?>">
+							<button type='submit' name='setAlumno' class='btn btn-link'>Establecer</button>
 						</div>
 					</div>
 					<div class="control-group">
 						<label for="nombre" class='control-label'>Nombre</label>
 						<div class="controls">
-							<input type="text" name='nombre' id='nombre' class='span5' placeholder='Nombre' disabled >
+							<input type="text" name='nombre' id='nombre' class='span5' 
+							value="<?php if(isset($alumno_pago)){ echo $alumno_pago['nombre'].' '.$alumno_pago['apellidopat'].' '.$alumno_pago['apellidomat']; } ?>" disabled >
 						</div>
 					</div>
 					<div class="control-group">
 						<label for="carrera" class='control-label'>Carrera</label>
 						<div class="controls">
-							<input type="text" id='carrera' name='carrera' class='span5' placeholder='Carrera' disabled>
+							<input type="text" id='carrera' name='carrera' class='span5' placeholder='Carrera' disabled values='Hola'>
 						</div>
 					</div>
 					<div class="control-group">
